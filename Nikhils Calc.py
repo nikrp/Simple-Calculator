2321ae810f8ca921f4961998672020f590a92db5
nikrp <76831568+nikrp@users.noreply.github.com>
2021-02-09 15:13:11 -0800

    Added exponentiation!!!

--- Nikhils Calc.py
+++ Nikhils Calc.py
@@ -1,12 +1,12 @@
 # This is an improved version of my old calculator.
 
 def greeting():
-    print("    This is Nikhil, and I made this python calculator. It took some help, but I")
+    print("This is Nikhil, and I made this python calculator. It took some help, but I")
     print("managed to finish it in two days, I hope you like my calculator!!\n")
 
 def instructions():
-    print("    This calculator can only do addition, subtraction, multiplication,")
-    print("and division.It can only do one operation at a time, so it can't do")
+    print("This calculator can only do addition, subtraction, multiplication,")
+    print("and division. It can only do one operation at a time, so it can't do")
     print("num + num / num * num. It can do operations like num + num + num and")
     print("num * num * num.\n")
 
@@ -14,32 +14,37 @@ greeting()
 
 instructions()
 
-print("Permitted operations - addition, subtraction, division, and multiplication!\n")
+print("Permitted operations - addition, subtraction, division, multiplication, and exponentiation!\n")
 userOperation = input("Which operation would you like to use?: ")
-print("")
-print("Enter the nubers of your equation (e.g. 100 20 30): ", end="")
-myList = list(map(int, input().split()))
 
 total = 0
 if userOperation.lower() in "addition add +":
+    print("Enter the nubers of your equation (e.g. 100 20 30): ", end="")
+    myList = list(map(int, input().split()))
     for entry in myList:
         total = total + entry
-    print(total)
+
 elif userOperation.lower() in "subtraction subtract -":
+    print("Enter the nubers of your equation (e.g. 100 20 30): ", end="")
+    myList = list(map(int, input().split()))
     for entry in myList:
         if total == 0:
             total = entry
         else:
             total = total - entry
-    print(total)
+
 elif userOperation.lower() in "multiplication multiply * x":
+    print("Enter the nubers of your equation (e.g. 100 20 30): ", end="")
+    myList = list(map(int, input().split()))
     for entry in myList:
         if total == 0:
             total = entry
         else:
             total = total * entry
-    print(total)
+
 elif userOperation.lower() in "division divide /":
+    print("Enter the nubers of your equation (e.g. 100 20 30): ", end="")
+    myList = list(map(int, input().split()))
     for entry in myList:
         if total == 0:
             total = entry
@@ -48,10 +53,22 @@ elif userOperation.lower() in "division divide /":
                 print("Oops! Can't divide by zero!")
             else:
                 total = total / entry
-    print(total)
 
+elif userOperation.lower() in "exponentiation exponents powers ^":
+    num = int(input("Enter the number: "))
+    expo = int(input("Enter the exponent: "))
+    for entry in range(0, expo):
+        if total == 0:
+            total = num
+        else:
+            total = total * num
+    if expo == 0:
+        total = 1
+
+print(total)
+    
 def goodbye():
-    print("    I hoped you like my calculator! It took two days and lots of help to make!")
+    print("\nI hoped you like my calculator! It took two days and lots of help to make!")
     print("I will probably make an improved calculator in the future which can do powers!")
     print("THIS IS NIKHIL'S CALCULATOR!!!")
 
