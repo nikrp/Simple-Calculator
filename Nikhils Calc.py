a0637cceafdec114cf593185f0511a75938ea021
Nikhil Pellakuru <nikpellakuru@gmail.com>
2021-02-12 22:25:21 -0800

    Added comparison category and made decimals work with

--- Nikhils Calc.py
+++ Nikhils Calc.py
@@ -1,52 +1,64 @@
-# This is an improved version of my old calculator.
+# This is my calculator
 
+# Setting the total function.
 total = 0
 
 def greeting():
-    print("This is Nikhil, and I made this python calculator. It took some help, but I")
-    print("managed to finish it in two days, I hope you like my calculator!!\n")
+    print("\tHello, my name is Nikhil and this is my calculator! This calculator took")
+    print("a while to make, but I finally did it with some help! I hope you like my")
+    print("calculator!\n\n")
 
 def instructions():
-    print("This calculator can only do addition, subtraction, multiplication,")
-    print("division, and exponentiation. It can only do one operation at a time,")
-    print("so it can't do \"num + num / num * num\". It can do operations like ")
-    print("num + num + num and num * num * num.\n")
+    print("\tMy calculator can do addition, subtraction, multiplication, division,")
+    print("exponentiation, comparison, decimals, and a surprise category! My calculator")
+    print("can't do multiple operation at the same time(e.g. 100 + 20 / 50 * 30 - 1).")
+    print("My calculator can only do one operation at a time(e.g. 100 + 100 + 387).")
+    print("More information below!\n\n")
 
+def printVal(val):
+    if val % int(val) == 0:
+        val = int(val)
+    else:
+        val = round(val, 2)
+    return str(val)
 
+# Addition category
 def addition():
     global total
     newTotal = ""
-    print("Enter the nubers of your equation (e.g. 100 20 30): ", end="")
-    myList = list(map(int, input().split()))
+    print("Enter the numbers of your equation (e.g. 100 20 30): ", end="")
+    myList = list(map(float, input().split()))
     for entry in myList:
         total = total + entry
         if len(newTotal) != 0:
-            newTotal = newTotal + " + " + str(entry)
+            newTotal = newTotal + " + " + printVal(entry)
         else:
-            newTotal = str(entry)
-    total = f"{newTotal} = {total}"
+            newTotal = printVal(entry)
+    total = f"{newTotal} = {printVal(total)}"
 
+# Subtraction category
 def subtraction():
     global total
     newTotal = ""
     print("Enter the nubers of your equation (e.g. 100 20 30): ", end="")
-    myList = list(map(int, input().split()))
+    myList = list(map(float, input().split()))
     for idx in range(0, len(myList)):
         if idx == 0:
             total = myList[idx]
         else:
             total = total - myList[idx]
         if len(newTotal) != 0:
-            newTotal = newTotal + " - " + str(myList[idx])
+            newTotal = newTotal + " - " + printVal(myList[idx])
         else:
-            newTotal = str(myList[idx])
-    total = f"{newTotal} = {total}"
+            newTotal = printVal(myList[idx])
+    total = f"{newTotal} = {printVal(total)}"
 
+# Multiplication category
 def multiplication():
     global total
     newTotal = ""
     print("Enter the nubers of your equation (e.g. 100 20 30): ", end="")
-    myList = list(map(int, input().split()))
+    myList = list(map(float, input().split()))
     for i in range(0, len(myList)):
         # If it is the first entry
         if i == 0:
@@ -54,16 +66,20 @@ def multiplication():
         else:
             total = total * myList[i]
         if len(newTotal) != 0:
-            newTotal = newTotal + " * " + str(myList[i])
+            newTotal = newTotal + " * " + printVal(myList[i])
         else:
-            newTotal = str(myList[i])
-    total = f"{newTotal} = {total}"
+            newTotal = printVal(myList[i])
+    total = f"{newTotal} = {printVal(total)}"
 
+
+
+# Division category
 def division():
     global total
     newTotal = ""
+    isZeroDiv = False
     print("Enter the nubers of your equation (e.g. 100 20 30): ", end="")
-    myList = list(map(int, input().split()))
+    myList = list(map(float, input().split()))
     for i in range(0, len(myList)):
         if i == 0:
             total = myList[i]
@@ -74,14 +90,15 @@ def division():
             else:
                 total = total / myList[i]
         if len(newTotal) != 0:
-            newTotal = newTotal + " / " + str(myList[i])
+            newTotal = newTotal + " / " + printVal(myList[i])
         else:
-            newTotal = str(myList[i])
+            newTotal = printVal(myList[i])
     if isZeroDiv == False:
-        total = f"{newTotal} = {total}"
+        total = f"{newTotal} = {printVal(total)}"
     else:
         total = "Can't divide by 0!!"
 
+# Exponentiation category
 def exponents():
     global total
     num = int(input("Enter the number: "))
@@ -97,6 +114,7 @@ def exponents():
         total = 1
     '''
 
+# Fibonacci numbers for surprise category
 def isFib(num):
     n1 = 0
     n2 = 1
@@ -116,19 +134,23 @@ def isFib(num):
         n1 = n2
         n2 = temp
     if isFibNum == True:
-        print(f"{num} is a fibonacci number!")
+        print(f"  is a fibonacci number!")
     else:
-        print(f"{num} is not a fibonacci number!")
-        
+        print(f"  is not a fibonacci number!")
+
+# Surprise category        
 def surprise():
     global total
     isPrime = True
     num = int(input("Pick a number: "))
+    print(f"{num}:")
+    # 1. Check for even or odd
     if num % 2 == 0:
-        print(f"{num} is an even number!")
+        print(f"  is an even number!")
     else:
-        print(f"{num} is an odd number!")
-
+        print(f"  is an odd number!")
+    
+    # 2. Check if num is in the fibonacci numbers
     isFib(num)
 
     # 3. Check for prime or composite number
@@ -136,21 +158,63 @@ def surprise():
         if num % i == 0:
             isPrime = False
             break
-    if num < 0:
-        total = f"Not checking {num} for prime or composite test!"
-    elif num == 1 or num == 0:
-        total = f"{num} is neither prime or composite!"
+    if num < -1:
+        total = f"  is composite!"
+    elif num == 1 or num == 0 or num == -1:
+        total = f"  is neither prime or composite!"
     elif isPrime == False:
-        total = f"{num} is a composite number!"
+        total = f"  is a composite number!"
     else:
-        total = f"{num} is a prime number!"
-            
+        total = f"  is a prime number!"
 
+# Compare category
+def compare():
+    global total
+    print("\nAvalible comparisons:\n\t1. Greater Than\n\t2. Less Than\n\t3. Equal To\n\t4. Greater Than Or Equal To\n\t5. Less Than Or Equal To\n")
+    compareOp = int(input("Pick the number that equals the comparison! 1, 2, 3, 4, or 5: "))
+    n1 = int(input("Pick your first number: "))
+    n2 = int(input("Pick your second number: "))
+    if compareOp == 1:
+        newTotal = n1 > n2
+        total = f"{n1} > {n2} = {newTotal}"
+    elif compareOp == 2:
+        newTotal = n1 < n2
+        total = f"{n1} < {n2} = {newTotal}"
+    elif compareOp == 3:
+        newTotal = f"{n1 == n2}"
+        total = f"{n1} = {n2} = {newTotal}"
+    elif compareOp == 4:
+        newTotal = n1 >= n2
+        total = f"{n1} >= {n2} = {newTotal}"
+    elif compareOp == 5:
+        newTotal = n1 <= n2
+        total = f"{n1} <= {n2} = {newTotal}"
+
+'''def decimal():
+    global total
+    newTotal = ""
+    print("\nAvalible Operations:\n\t1. Addition\n\t2. Subtraction\n\t3. Multiplication\n\t4. Division\n")
+    deciOper = int(input("Pick the number that equals the decimal operation! 1, 2, 3, or 4: "))
+    print("\nEnter the numbers in your operation(e.g. 1.00 1.25 0.48): ", end="")
+    myList = list(map(float, input().split()))
+    print(myList)
+    if deciOper == 1:
+        for i in myList:
+            total = total + i
+        if len(newTotal) != 0:
+            newTotal = newTotal + " + " + str(i)
+        else:
+            newTotal = str(i)
+    total = f"{newTotal} = {total}"'''
+        
+
+# Goodbye function to print after operation is completed
 def goodbye():
-    print("\nI hoped you like my calculator! It took two days and lots of help to make!")
-    print("I will probably make an improved calculator in the future!")
+    print("\n\tI hoped you like my calculator! It took two days and lots of help to")
+    print("make! I will probably make an improved calculator in the future!")
     print("THIS IS NIKHIL'S CALCULATOR!!!")
 
+# Run function, what happens when code runs
 def run():
     global total
     greeting()
@@ -158,23 +222,26 @@ def run():
     
     total = 0
     
-    print("Pick an operation:\n\t1. Addition\n\t2. Subtraction\n\t3. Division\n\t4. Multiplication\n\t5. Exponentiation\n\t6. Surprise!!")
-    userOperation = input("Pick the number that equals the operation, 1, 2, 3, 4, 5, or 6: ")
+    print("Pick an operation:\n\t1. Surprise!\n\t2. Addition\n\t3. Subtraction\n\t4. Multiplication\n\t5. Division\n\t6. Exponentiation\n\t7. Comparison\n")
+    userOperation = input("Pick the number that equals the operation, 1, 2, 3, 4, 5, 6, or 7: ")
     if userOperation.lower() == "1":
-        addition()
+        surprise()
     elif userOperation.lower() == "2":
-        subtraction()
+        addition()
     elif userOperation.lower() == "3":
-        division()
+        subtraction()
     elif userOperation.lower() == "4":
         multiplication()
     elif userOperation.lower() == "5":
-        exponents()
+        division()
     elif userOperation.lower() == "6":
-        surprise()
+        exponents()
+    elif userOperation.lower() == "7":
+        compare()
     else:
-        total = "Invalid operation, pick [1-6]!"
+        total = "Invalid operation, pick [1-7]!"
 
+# Main function, what to run
 if __name__ == "__main__":
     run()
     print(total)
